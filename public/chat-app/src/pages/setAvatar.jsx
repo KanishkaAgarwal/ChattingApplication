01031d1ac52622a9890c41702ebec398ab6192b8
KanishkaAgarwal <kanishka.agarwal208@gmail.com>
2025-05-09 17:01:02 +0530

    Changes in setAvatar

--- public/chat-app/src/pages/setAvatar.jsx
+++ public/chat-app/src/pages/setAvatar.jsx
@@ -9,7 +9,7 @@ import { setAvatarRoute } from "../utils/APIRoutes";
 import {Buffer} from 'buffer';
 
 export default function SetAvatar() {
-    const api = "https://api.multiavatar.com";
+    //const api = `https://api.dicebear.com/7.x/bottts/svg?seed=${Math.round(Math.random() * 1000)}`;
     const navigate = useNavigate();
     const [avatars, setAvatars] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -21,20 +21,59 @@ export default function SetAvatar() {
         draggable: true,
         theme: "dark",
     }
-    const setProfilePicture = async() => {};
+
+    useEffect(() => {
+          if(!localStorage.getItem('chat-app-user')){
+            navigate('/login')
+          }
+    }, [])
+    
+    const setProfilePicture = async() => {
+        if(selectedAvatar === undefined){
+            toast.error("Please select an Avatar", toastOptions);
+        }
+
+        else{
+            const user = await JSON.parse(localStorage.getItem("chat-app-user"));
+            const {data} = await axios.post(`${setAvatarRoute}/${user._id}`, {
+                image: avatars[selectedAvatar],
+            });
+
+            if(data.isSet){
+                user.isAvatarImageSet = true;
+                user.avatarImage = data.image;
+                localStorage.setItem("chat-app-user", JSON.stringify(user));
+                navigate("/");
+            }
+
+            else{
+                toast.error("Error setting avatar. Please try again", toastOptions);
+            }
+        }
+    };
     useEffect(() => {
         const fetchAvatars = async () => {
             const data = [];
             for(let i=0; i<4; i++){
-                const image = await axios.get(
-                    `${api}/${Math.round(Math.random() * 1000)}`, {
-                        headers: {
-                            'Accept' : 'image/svg+xml'
-                        }
-                    }
-                );
-                const buffer = new Buffer(image.data);
-                data.push(buffer.toString("base64"));
+                const id = Math.floor(Math.random() * 1000);
+                data.push(`https://api.dicebear.com/7.x/bottts/svg?seed=${id}`);
+                //data.push( `https://api.dicebear.com/7.x/bottts/svg?seed=${Math.round(Math.random() * 1000)};`);
+                // const image = await axios.get(
+                //     `${api}/${Math.round(Math.random() * 1000)}`, {
+                //         headers: {
+                //             'Accept' : 'image/svg+xml'
+                //         }
+                //     }
+                // );
+                // const base64 = btoa(image.data);
+                // data.push(base64);
+                // try {
+                //     const response = await axios.get("https://jsonplaceholder.typicode.com/invalidEndpoint");
+                //     console.log(response.data); // You should see an error in the console
+                // } catch (error) {
+                //     console.error("Error fetching data:", error);
+                //     setIsLoading(false); // stop loader once error occurs
+                // }
             }
             setAvatars(data);
             setIsLoading(false);
@@ -43,34 +82,120 @@ export default function SetAvatar() {
     }, []);
   return (
     <>
-        <Container>
-            <div className="title-container">
-                <h1>Pick an AVATAR for your Profile Picture</h1>
-            </div>
-            <div className="avatars">{
-                avatars.map((avatar, index) => {
-                    return(
-                       
-                        <div
-                            key={index} 
-                            className={`avatar ${
-                            selectedAvatar === index ? "selected" : ""
-                        }`}>
+    {
+        isLoading ? (
+            <LoaderContainer> 
+                <img src={loader} alt="loader" className="loader"/>
+            </LoaderContainer>
+        ) : (
+            <Container>
+                <div className="title-container">
+                    <h1>Pick an AVATAR for your Profile Picture</h1>
+                </div>
+                <div className="avatars">{
+                    avatars.map((avatar, index) => {
+                        return(
+                        
+                            <div
+                                key={index} 
+                                className={`avatar ${selectedAvatar === index ? "selected" : ""}`}
+                                onClick={() => setSelectedAvatar(index)}
+                            >
 
-                            <img
-                              src={`data:image/svg+xml;base64,${avatar}`}
-                              alt="avatar"
-                              onClick={() => setSelectedAvatar(index)}
-                            />
+                                <img src={avatar} alt="avatar" />
 
-                        </div>
-                    )
-                })
-            }</div>
-        </Container>
+                            </div>
+                        )
+                    })
+                }</div>
+                <button className="submit-btn" onClick={setProfilePicture}>Set as Profile Picture</button>
+           </Container>
+        )
+    }
+        
         <ToastContainer/>
     </>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   flex-direction: column;
+   gap: 3rem;
+   background-color: #131324;
+   height: 100vh;
+   width: 100vw;
+
+   .loader{
+      max-inline-size: 100%;
+   }
+
+   .title-container{
+      h1{
+        color: white;
+      }
+   }
+
+   .avatars{
+      display: flex;
+      gap: 2rem;
+
+      .avatar{
+        border: 0.4rem solid transparent;
+        padding: 0.4rem;
+        border-radius: 5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: 0.2s step-start;
+        img{
+           height: 6rem;
+        }
+      }
+
+      .selected{
+            border: 0.4rem solid  #4e0eff
+        }
+    }
+    
+    .submit-btn{
+        background-color: #997af0;
+        color: white;
+        padding: 1rem 2rem;
+        border: none;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 0.4rem;
+        font-size: 1rem;
+        text-transform: uppercase;
+        transition: 0.2s ease-in-out;
+
+        &:hover{
+            background-color: #4e0eff;
+        }
+    }
+
+    .loader{
+       height: 100px;
+       width: 100px;
+    }
+
+   
+`;
+
+const LoaderContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #131324;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .loader {
+    height: 100px;
+    width: 100px;
+  }
+`;
+
